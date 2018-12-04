1c3f7e2e9e850fc55f0478bc9ac5182ba7c90bab
lilla tari <tari.lilla2@gmail.com>
2018-12-04 09:34:19 +0100

    Projekt létrejott

---
+++ EgeszSzamok/EgeszSzamok/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EgeszSzamok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();    
+        }
+        
+    }
+}
